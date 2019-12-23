4fcf9b8628fb2be6b4d32bee75ab5a899ab85e5a
tanvesh <sarvetanvesh01@gmail.com>
2019-12-23 17:00:58 +0530

    first commit from laptop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Person from "./Person/Person";
+import "./App.css";
 
-function App() {
+
+const App = props => {
+  const [personState, setpersonState] = useState({
+    person: [
+      { name: "Tanvesh01", age: 18 },
+      { name: "yoyoHoney!!! ", age: 35 },
+      { name: "Tanvesh012", age: 185 },
+    ]
+  });
+
+  const [toogleState, setToggleState] = useState({
+    showPerson: false
+  })
+
+  const [OtherState, setOtherState] = useState("iphone");
+
+  
+  const nameChangeHandler = (event) => {
+    setpersonState({
+      person: [
+        { name: "Tanvesh", age: 50 },
+        { name: event.target.value, age: 39 }
+      ]
+    });
+  };
+
+  const deleteNameHandler = (personIndex) => {
+    const person = personState.person;
+    person.splice(personIndex, 1);
+    setpersonState({person:person});
+  }
+
+  const toogleNames = () => {
+    if (toogleState.showPerson === true) {
+      setToggleState({showPerson: false})
+    } else {
+      setToggleState({showPerson: true})
+    }
+  }
+
+  let persons = null;
+
+  if(toogleState.showPerson){
+    persons =  (
+      <div>
+        {personState.person.map((person, index) => {
+          return (
+          <Person 
+          name = {person.name}
+          age = {person.age}
+          click = {() => deleteNameHandler(index)}
+          />
+          )
+        })}  
+      </div>
+    )
+  }  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>this is it!</p>
+      <button onClick={toogleNames}>
+        Switch this
+      </button>
+      {persons}
+      {/* { toogleState.showPerson === true ? 
+      null
+      } */}
+      <p className="App">{OtherState}</p>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Person = props => {
+  console.log(props.click)
+  return (
+    <div className="App">
+      
+      <p onClick = {props.click}>
+        hii this is {props.name} and my age is {props.age}
+      </p>
+      <input type="text" onChange={props.changeName} />
+    </div>
+    
+  );
+};
+
+export default Person;
