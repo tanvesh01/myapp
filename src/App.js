78b6a26c529f74bb6c19b0cd255876869f01b607
tanvesh01 <sarvetanvesh01@gmail.com>
2019-12-23 20:34:16 +0530

    commit from dell pc

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import "./App.css";
 const App = props => {
   const [personState, setpersonState] = useState({
     person: [
-      { name: "Tanvesh01", age: 18 },
-      { name: "yoyoHoney!!! ", age: 35 },
-      { name: "Tanvesh012", age: 185 },
+      { id:'asdas' , name: "Tanvesh01", age: 18 },
+      { id:'ysyrh', name: "yoyoHoney!!! ", age: 35 },
+      { id:'ysysy', name: "Tanvesh012", age: 185 },
     ]
   });
 
@@ -19,7 +19,14 @@ const App = props => {
   const [OtherState, setOtherState] = useState("iphone");
 
   
-  const nameChangeHandler = (event) => {
+  const nameChangeHandler = (event, id) => {
+    const personIndex = personState.person.findIndex(p =>{
+      return p.id === id;
+    })
+    const person = {
+      ...personState.person[personIndex]
+    }
+    
     setpersonState({
       person: [
         { name: "Tanvesh", age: 50 },
@@ -50,9 +57,11 @@ const App = props => {
         {personState.person.map((person, index) => {
           return (
           <Person 
+          key = {person.id}
           name = {person.name}
           age = {person.age}
           click = {() => deleteNameHandler(index)}
+          changeName = {(event) => nameChangeHandler(event, person.id)} 
           />
           )
         })}  
