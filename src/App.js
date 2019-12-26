323b3dca4bcb28ec0f8403a66975224db92ddb1b
tanvesh <sarvetanvesh01@gmail.com>
2019-12-26 23:11:38 +0530

    oopsie

--- src/App.js
+++ src/App.js
@@ -2,34 +2,30 @@ import React, { useState } from "react";
 import Person from "./Person/Person";
 import "./App.css";
 
-
 const App = props => {
   const [personState, setpersonState] = useState({
     person: [
-      { id:'asdas' , name: "Tanvesh01", age: 18 },
-      { id:'ysyrh', name: "yoyoHoney!!! ", age: 35 },
-      { id:'ysysy', name: "Tanvesh012", age: 185 },
+      { id: "asdas", name: "Tanvesh01", age: 18 },
+      { id: "ysyrh", name: "yoyoHoney!!! ", age: 35 },
+      { id: "ysysy", name: "Tanvesh012", age: 185 }
     ]
   });
-  
+
   const [toogleState, setToggleState] = useState({
     showPerson: false
-  })
+  });
 
   const [OtherState, setOtherState] = useState("iphone");
 
-  
   const nameChangeHandler = (event, id) => {
-    const personIndex = personState.person.findIndex(p =>{
+    const personIndex = personState.person.findIndex(p => {
       return p.id === id;
-    })
+    });
     const person = {
       ...personState.person[personIndex]
-    }
+    };
     person.name = event.target.value;
-    const Person = [
-      ...personState.person
-    ]
+    const Person = [...personState.person];
     Person[personIndex] = person;
 
     setpersonState({
@@ -37,44 +33,49 @@ const App = props => {
     });
   };
 
-  const deleteNameHandler = (personIndex) => {
+  const deleteNameHandler = personIndex => {
     const person = personState.person;
     person.splice(personIndex, 1);
-    setpersonState({person:person});
-  }
+    setpersonState({ person: person });
+  };
 
   const toogleNames = () => {
     if (toogleState.showPerson === true) {
-      setToggleState({showPerson: false})
+      setToggleState({ showPerson: false });
     } else {
-      setToggleState({showPerson: true})
+      setToggleState({ showPerson: true });
     }
-  }
+  };
 
   let persons = null;
-
-  if(toogleState.showPerson){
-    persons =  (
+  const myStyle = {
+    background: "green",
+    color: "white",
+    padding: "5px"
+  };
+  if (toogleState.showPerson) {
+    persons = (
       <div>
         {personState.person.map((person, index) => {
           return (
-          <Person 
-          key = {person.id}
-          name = {person.name}
-          age = {person.age}
-          click = {() => deleteNameHandler(index)}
-          changeName = {(event) => nameChangeHandler(event, person.id)} 
-          />
-          )
-        })}  
+            <Person
+              key={person.id}
+              name={person.name}
+              age={person.age}
+              click={() => deleteNameHandler(index)}
+              changeName={event => nameChangeHandler(event, person.id)}
+            />
+          );
+        })}
       </div>
-    )
-  }  
+    );
+    myStyle.background = "red";
+  }
 
   return (
     <div className="App">
       <p>this is it!</p>
-      <button onClick={toogleNames}>
+      <button style={myStyle} onClick={toogleNames}>
         Switch this
       </button>
       {persons}
